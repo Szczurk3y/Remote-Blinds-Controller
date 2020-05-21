ce7446c2956e430af06c256d50ef8bc790429778
maciek stoiński <maciek.stoinski2@gmail.com>
2020-05-21 12:35:28 +0200

    Fix HOST address bug

--- app/src/main/java/com/szczurk3y/blindsanimation/UDP.kt
+++ app/src/main/java/com/szczurk3y/blindsanimation/UDP.kt
@@ -1,21 +1,23 @@
 package com.szczurk3y.blindsanimation
 
 import android.app.Activity
+import android.app.PendingIntent.getBroadcast
 import android.content.Context
+import android.content.Context.WIFI_SERVICE
 import android.net.wifi.WifiManager
 import android.os.AsyncTask
+import android.text.format.Formatter
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import java.io.IOException
 import java.lang.Exception
-import java.net.DatagramPacket
-import java.net.DatagramSocket
-import java.net.InetAddress
+import java.net.*
 
 class UDP(val activity: Activity): Runnable {
     companion object {
         const val PORT = 10107
-        val HOST = "192.168.1.255"
+        var HOST = ""
     }
 
     override fun run() {
@@ -23,28 +25,50 @@ class UDP(val activity: Activity): Runnable {
         val lock: WifiManager.MulticastLock = wifiManager.createMulticastLock("Log_Tag")
         lock.acquire()
 
-        try {
-            val messageBuffer = ByteArray(1024)
-            val datagramSocket = DatagramSocket(PORT, InetAddress.getByName(HOST))
-            val datagramPacket = DatagramPacket(messageBuffer, messageBuffer.size)
-            while (true) {
-                Log.i("UDP_LOG", "elo przed")
-                datagramSocket.receive(datagramPacket)
-                Log.i("UDP_LOG", "elo po")
-                val text: String? = String(messageBuffer, 0, datagramPacket.length)
-                val ip: String? = datagramPacket.address.toString().removeRange(0..0)
-                val blind = Blind(
-                    id = Handler.blindsList.size,
-                    name = ip,
-                    itemProgression = Handler.blindsList.size,
-                    ip = ip.toString()
-                )
-                activity.runOnUiThread {
-                    Handler.checkAndAdd(blind, text, false)
+        HOST = getBroadcast()
+        Log.i("Broadcast:", HOST)
+        if (HOST != "Disconnected") {
+            try {
+                while (true) {
+                    val messageBuffer = ByteArray(1024)
+                    val datagramSocket = DatagramSocket(PORT, InetAddress.getByName(HOST))
+                    val datagramPacket = DatagramPacket(messageBuffer, messageBuffer.size)
+                    while (true) {
+                        datagramSocket.receive(datagramPacket)
+                        val text: String? = String(messageBuffer, 0, datagramPacket.length)
+                        val ip: String? = datagramPacket.address.toString().removeRange(0..0)
+                        val blind = Blind(
+                            id = Handler.blindsList.size,
+                            name = ip,
+                            itemProgression = Handler.blindsList.size,
+                            ip = ip.toString()
+                        )
+                        activity.runOnUiThread {
+                            Handler.checkAndAdd(blind, text, false)
+                        }
+                    }
                 }
+            } catch (e: Exception) {
+                Log.i("UDP_ERROR", e.printStackTrace().toString())
             }
-        } catch (e: Exception) {
-            Log.i("UDP_ERROR", e.printStackTrace().toString())
         }
     }
+
+    @Throws(Exception::class)
+    fun getBroadcast(): String {
+        val wm = activity.baseContext.getSystemService(WIFI_SERVICE) as WifiManager
+        val ip = Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
+        return if (ip != "0.0.0.0") {
+            var broadcast = ""
+            var counterOfDots = 0
+            for (letter in ip) {
+                if (counterOfDots < 3) broadcast += letter
+                if (letter == '.') counterOfDots += 1
+            }
+            broadcast+"255" // x.x.x.255 is a broadcast address
+        } else {
+            "Disconnected"
+        }
+    }
+
 }
